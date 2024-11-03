ddb468cd9caf520f74760b56ada255337f8ec1f5
Sidharth <sidharthinfernal@gmail.com>
2024-11-03 19:31:18 +0530

    init

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from "better-auth/react";
 
 
 export const { signIn, signOut, signUp, useSession } = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL!,
+   baseURL: "http://localhost:3000"
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-"use client"
-
-import { signIn,useSession } from "@/app/lib/auth-client";
-import { redirect } from "next/navigation";
 
 export default function Home() {
-  const session = useSession();
-  if(session.data?.user){
-    redirect("/send")
-  }
+  
   return (
     <div>
-      <button
-        onClick={async () => {
-          await signIn.social({
-            provider: "github",
-          });
-        }}
-      >Sign in</button>
+     <h1>hloo</h1>
     </div>
   );
 }
